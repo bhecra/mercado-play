2a17d7006243b91c6b1585a0a067a1c8cd3993cf
cburbano <cburbano@ubits.co>
2024-01-26 23:14:49 -0500

    add fonts and add favicon

--- src/App.tsx
+++ src/App.tsx
@@ -4,6 +4,10 @@ function App() {
 
   return (
     <>
+
+    <h1>Hola mundo probando Meli play</h1>
+    <h2>Hola mundo probando Meli play</h2>
+    <p>Hola mundo probando Meli play</p>
       
     </>
   )
