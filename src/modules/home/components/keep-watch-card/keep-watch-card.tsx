90818e1257ce4a7437934faa597ef148607cfd9b
cburbano <cburbano@ubits.co>
2024-01-29 11:03:09 -0500

    keep watch

--- src/modules/home/components/keep-watch-card/keep-watch-card.tsx
+++ src/modules/home/components/keep-watch-card/keep-watch-card.tsx
@@ -1,29 +1,31 @@
 const KeepWatchCard = () => {
 	return (
 		<section className="carousel-img">
-			<a href='/player' target='_blank' rel='noopener noreferrer'>
+			<a href='/player' target='_blank' rel='noopener noreferrer' className="redirect-multimedia">
 				<img
 					src='https://musicart.xboxlive.com/7/56322a00-0000-0000-0000-000000000002/504/image.jpg?w=1920&h=1080'
 					alt=''
-          className='image-ratio'
+                 className='image-ratio image-keep'
 				/>
-				<div>2h 3m por ver</div>
+				<div className='subtitle-keep'>2h 3m por ver</div>
 			</a>
-			<a href='/player' target='_blank' rel='noopener noreferrer'>
+
+			<a href='/player' target='_blank' rel='noopener noreferrer' className="redirect-multimedia">
 				<img
 					src='https://musicart.xboxlive.com/7/56322a00-0000-0000-0000-000000000002/504/image.jpg?w=1920&h=1080'
 					alt=''
-          className='image-ratio'
+                 className='image-ratio image-keep'
 				/>
-				<div>2h 3m por ver</div>
+				<div className='subtitle-keep'>2h 3m por ver</div>
 			</a>
-			<a href='/player' target='_blank' rel='noopener noreferrer'>
+
+			<a href='/player' target='_blank' rel='noopener noreferrer' className="redirect-multimedia">
 				<img
 					src='https://musicart.xboxlive.com/7/56322a00-0000-0000-0000-000000000002/504/image.jpg?w=1920&h=1080'
 					alt=''
-          className='image-ratio'
+                 className='image-ratio image-keep'
 				/>
-				<div>2h 3m por ver</div>
+				<div className='subtitle-keep'>2h 3m por ver</div>
 			</a>
 		</section>
 	)
