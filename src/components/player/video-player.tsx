a0fea4ea60db25f46acfc9d5201de51a2e2a6508
cburbano <cburbano@ubits.co>
2024-01-29 11:03:58 -0500

    formatt code

--- src/components/player/video-player.tsx
+++ src/components/player/video-player.tsx
@@ -14,7 +14,6 @@ const VideoPlayerComponent = ({ sourceFile }) => {
 
 
   const handlePlay = () => {
-
     if (videoRef) {
       if (playing) {
         videoRef.current?.pause()
@@ -31,19 +30,13 @@ const VideoPlayerComponent = ({ sourceFile }) => {
     }
   }
 
-
   const muted = () => {
-
-    console.log(videoRef.current.muted);
-
     if (videoRef) {
-
       videoRef.current.muted = !videoRef.current.muted
     }
   }
+
   return (
-
-
     <div className="meli-player">
       <video
         autoPlay={true}
@@ -51,7 +44,7 @@ const VideoPlayerComponent = ({ sourceFile }) => {
         width={"100%"}
         src={sourceFile || '/videos/leo-dan.webm'}
         onError={onError}
-        
+
       >
         No se ecnotró el video <code>video</code>.
       </video>
@@ -61,7 +54,7 @@ const VideoPlayerComponent = ({ sourceFile }) => {
         <button id="mute" onClick={muted}>Mute</button>
         {/* <button id="full-screen" onClick={fullScreenButton}>Fullscreen</button> */}
         <div className="time-indicator">0:00 / 0:00</div>
-      
+
         <button id="volume">🔊</button>
 
         <button id="full-screen" onClick={fullScreenButton}>⛶</button>
